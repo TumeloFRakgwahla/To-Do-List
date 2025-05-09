764ac9546f197ebc86c2b116856e9fe438a3ff45
TumeloFRakgwahla <tumelorakgwahla@gmail.com>
2025-05-10 00:27:29 +0200

    Adding Style to the todo page and fixing the checkbox

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,32 +8,16 @@ namespace TodoListApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly TodoContext _context;
-        public IndexModel(TodoContext context)
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
         {
-            _context = context;
+            _logger = logger;
         }
-
-        public List<Todo> Todos { get; set; } = new();
-
-        [BindProperty]
-        public Todo NewTodo { get; set; }
 
         public void OnGet()
         {
-            Todos = _context.Todos.ToList();
+
         }
-
-        public IActionResult OnPost()
-        {
-            if (!string.IsNullOrWhiteSpace(NewTodo.Title))
-            {
-                _context.Todos.Add(NewTodo);
-                _context.SaveChanges();
-            }
-
-            return RedirectToPage();
-        }
-
     }
 }
---
+++ Pages/Todo.cshtml.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TodoListApp.Data;
+using TodoListApp.Models;
+
+namespace TodoListApp.Pages
+{
+    public class TodoModel : PageModel
+    {
+        private readonly TodoContext _context;
+        public TodoModel(TodoContext context)
+        {
+            _context = context;
+        }
+
+        public List<Todo> Todos { get; set; } = new();
+
+        [BindProperty]
+        public Todo NewTodo { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            Todos = await _context.Todos.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!string.IsNullOrWhiteSpace(NewTodo.Title))
+            {
+                _context.Todos.Add(NewTodo);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostToggleAsync(int id, bool isDone)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo != null)
+            {
+                todo.IsDone = isDone;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
+        }
+ 
+}
+}
